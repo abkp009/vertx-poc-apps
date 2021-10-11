f4f2d5c2c44570591027ade00fdbbe58f8f4be5d
Abhishek Kumar Pandey <abhishek.kumar.pandey@ericsson.com>
2021-10-11 16:19:35 +0530

    demo init

---
+++ nels-demo/src/test/java/com/example/nelsdemo/NelsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nelsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
